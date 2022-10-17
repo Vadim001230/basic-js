df854aa2f0d3d08e9aec959595d10b38e2e38cfc
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-10-18 00:31:47 +0300

    feat: complite matrix-elem-sum.js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -19,15 +19,15 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getMatrixElementsSum(matrix) {
   let sum = 0;
     for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-        if (matrix[i][j] === 0) {
-          i++
-        } else {
-          sum += matrix[i][j];
-        }
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        sum += matrix[i][j];
+      } else if (matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
       }
     }
-    return sum
+  }
+    return sum;
 }
 
 module.exports = {
